9b5a4721e8b87c614251a08241223018e838805d
Nicolas <n.henrydg@gmail.com>
2022-03-17 15:13:58 +0100

    Creation des components de la page experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SchoolExperienceComponent } from './school-experience/school-experience.component';
+import { SchoolComponent } from './school-experience/school/school.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { JobComponent } from './work-experience/job/job.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    AppComponent,
+    SchoolExperienceComponent,
+    SchoolComponent,
+    WorkExperienceComponent,
+    JobComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/school-experience/school-experience.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-school-experience',
+  templateUrl: './school-experience.component.html',
+  styleUrls: ['./school-experience.component.css']
+})
+export class SchoolExperienceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
