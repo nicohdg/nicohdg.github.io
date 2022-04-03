d8e3fed514fdacaae243eaf2a63b9341caed4535
Nicolas <n.henrydg@gmail.com>
2022-04-03 18:11:17 +0200

    tried animations

--- src/app/skills/informatique-skills/informatique-skills.component.ts
+++ src/app/skills/informatique-skills/informatique-skills.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { state, trigger, style, transition, animate } from '@angular/animations';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-informatique-skills',
   templateUrl: './informatique-skills.component.html',
-  styleUrls: ['./informatique-skills.component.css']
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: '#AA67B9',
+        width: '0px'
+      })),
+      state('full', style({
+        backgroundColor: '#314588',
+        width: '75%'
+      })),
+      transition('normal <=> full', animate(300))
+    ])
+  ]
 })
-export class InformatiqueSkillsComponent implements OnInit {
+export class InformatiqueSkillsComponent {
+  state = 'normal';
+
+  onAnimate() {
+    this.state == 'normal' ? this.state='full' : this.state='normal';
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
