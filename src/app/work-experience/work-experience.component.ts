2f43fde322b0e3c07f0878e662cc478618dd026c
Nicolas <n.henrydg@gmail.com>
2022-04-04 02:01:00 +0200

    Finished

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -16,7 +16,7 @@ export class WorkExperienceComponent implements OnInit {
       'Juin 2021',
       'Juillet 2021',
       'Familiarisation avec l\'environnement de développeur ; Programmation d\'un service de monitoring ; Découverte de la méthode Agile',
-      'https://tinyurl.com/yubfwccr'
+      'https://logos-download.com/wp-content/uploads/2016/04/BNP_Paribas_logo_logotype_emblem.png'
     ),
 
     new Work(
@@ -26,7 +26,7 @@ export class WorkExperienceComponent implements OnInit {
       'Juin 2019',
       'Juillet 2019',
       'Accueillir, orienter, et guider des clients',
-      'https://tinyurl.com/56c8jswp'
+      'https://scontent-cdt1-1.xx.fbcdn.net/v/t39.30808-6/224885085_286465016590387_7023806066208986351_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=27fnSzk2SnEAX9tnWg6&_nc_ht=scontent-cdt1-1.xx&oh=00_AT8Hm8Vi_nPR7HpQZhDiltd2PlJG-Z6VK4MSKtvJTAHjAg&oe=624AFE27'
     )
   ];
 
